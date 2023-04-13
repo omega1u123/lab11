39d89a1103e8b19b8aefe900a87ae887a96b7236
omega1u123 <gvardian2810@gmail.com>
2023-04-14 00:52:33 +0300

    edited ChooseCommunityScreen.kt

--- app/src/main/java/com/topic3/android/reddit/screens/ChooseCommunityScreen.kt
+++ app/src/main/java/com/topic3/android/reddit/screens/ChooseCommunityScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.topic3.android.reddit.R
 import com.topic3.android.reddit.routing.BackButtonAction
+
 import com.topic3.android.reddit.routing.RedditRouter
 import com.topic3.android.reddit.viewmodel.MainViewModel
 import kotlinx.coroutines.Job
